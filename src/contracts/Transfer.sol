3c708953a05cded8fb96bfd7eeceb440e6cc49eb
naftalimurgor <murgornaftali@gmailc.om>
2023-07-15 22:42:10 +0300

    Add ganache local rpc

---
+++ src/contracts/Transfer.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity >= 0.8.0;
+
+
+/**
+ * A sample contract to test transfer of Ether for EOA to Contract and Contract to owner.
+ */
+
+contract Transfer {
+    constructor() {
+        
+    }
+}
