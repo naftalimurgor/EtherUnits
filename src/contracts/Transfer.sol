203f97aa0680f76cc5ceb308a539d02f4c190b21
naftalimurgor <murgornaftali@gmailc.om>
2023-08-03 15:26:01 +0300

    add constructor

--- src/contracts/Transfer.sol
+++ src/contracts/Transfer.sol
@@ -1,13 +1,20 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity >= 0.8.0;
-
+pragma solidity >=0.8.0;
 
 /**
  * A sample contract to test transfer of Ether for EOA to Contract and Contract to owner.
  */
 
 contract Transfer {
+    address public owner;
+
+    event EtherCharged(address amountWei, address indexed from);
+
     constructor() {
-        
+        owner = payable(msg.sender);
+    }
+
+    function chargeEther() external {
+        // msg.value vs msg.sender stuff
     }
 }
